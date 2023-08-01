36daf921ae43470fb9d066086b2192361c4e3848
Salvador243 <salvador243gm@gmail.com>
2023-08-01 13:39:53 -0400

    Salvador_Garcia_Martinez - final project

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,26 +3,64 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{username: "root", password: "admin", token : ""}];
+//{username: user, password: pass, token : tkn}
 
-const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+const isValid = (username)=>{ 
+    users.forEach((index, value) => {
+        if(index.username == username)
+            return false;
+    });
+    users.push( {
+        username : username
+    });
+
+    return true;
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    let flag = false;
+    users.forEach((index, value ) => {
+        if(index.username == username && index.password == password){
+            const token = jwt.sign({ username }, "wordSecret", { expiresIn: '2h' });
+            index.token = token;
+            console.log(token)
+            flag =  true;
+        }
+    }); 
+    return flag;
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const { username, password } = req.body;
+    let auth = authenticatedUser(username, password);
+
+    if(auth){
+        return res.status(200).json({message: "Autenticated successfully!"});
+    }
+    return res.status(300).json({message: "Credentials are not valid"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn;
+    let { review } = req.body;
+    let username = req.username;
+
+    books[isbn].reviews[username] = review;
+    return res.status(300).json({message: "La reseña se modifico correctamente"});
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    let isbn = req.params.isbn;
+    let username = req.username;
+    if (books[isbn].reviews.hasOwnProperty(username)) {
+        delete books[isbn].reviews[username];
+        return res.status(200).json({ message: "Review deleted successfully" });
+    } else {
+        return res.status(404).json({ message: "Review not found for the user" });
+    }
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,38 +6,84 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required for registration" });
+  }
+  if (!isValid(username)){
+    return res.status(400).json({message: "This username already exist, try another"});
+  }  
+  
+  let user = users.filter(u => u.username === username);
+  user[0]["password"] = password
+  user[0]["token"] = ""
+  return res.status(200).json({message: "User was registerd successfully"});
 });
 
-// Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', async function (req, res) {
+    try {
+      return res.send(200, JSON.stringify(books));
+    } catch (error) {
+      return res.status(500).json({ message: "Error fetching book list" });
+    }
 });
+  
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/isbn/:isbn', async function (req, res) {
+    try{
+        let isbn = req.params.isbn;
+        let book = books[isbn]
+        return res.send(200, book);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching book list" });
+    }
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author',async function (req, res) {
+    try{
+        let autor = req.params.author
+        let _books = []
+      
+        for (const key in books) {
+          if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            if (book.author.includes(autor)) {
+              _books.push(book);
+            }
+          }
+        }
+        return res.send(200, _books);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching book list" });
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', async function (req, res) {
+    try{
+        let title = req.params.title
+        let _books = []
+        for (const key in books) {
+          if (books.hasOwnProperty(key)) {
+            const book = books[key];
+            if (book.title.includes(title)) {
+              _books.push(book);
+            }
+          }
+        }
+        return res.send(200, _books);
+    } catch (error) {
+        return res.status(500).json({ message: "Error fetching book list" });
+    } 
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = req.params.isbn
+    let review = books[isbn].reviews
+    return res.send(200, review);
 });
 
 module.exports.general = public_users;
